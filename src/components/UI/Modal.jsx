676f90d62fa99a2585c49157fc4d0f303536f147
vvduong2k2 <duongnguyen2002.bmt@gmail.com>
2024-03-20 11:51:57 +0700

    add Modal to show cart

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function Modal({ children, open, className = "" }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    }
+
+    return () => dialog.current.close();
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog} className={`modal ${className}`}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
